c85b419e1d747192e4f335b0a38dd08a747eaf88
SurajGupta07 <bgsuraj07@gmail.com>
2024-01-22 13:34:45 +0530

    refactor: add nounces

--- xss/mitigation/index.js
+++ xss/mitigation/index.js
@@ -7,7 +7,7 @@ app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
     "default-src 'self';" +
-      "script-src 'self' 'unsafe-inline' https://unsecure.com;"
+      "script-src 'self' 'nonce-trustedKey' 'unsafe-inline' https://unsecure.com;"
   );
   next();
 });
